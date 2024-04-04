d72252b02fe6ad9fb3ad2a405ac6a9901357f56a
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2024-04-04 14:44:00 +0100

    [styling]

--- lib/CurrencyConverterMaterialPage.dart
+++ lib/CurrencyConverterMaterialPage.dart
@@ -4,29 +4,43 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return (const Scaffold(
+    return Scaffold(
         body: Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
+          const Text(
             '0',
             style: TextStyle(
                 fontWeight: FontWeight.bold, color: Colors.black, fontSize: 45),
           ),
-          TextField(
-            decoration: InputDecoration(
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: const TextField(
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+              decoration: InputDecoration(
                 hintText: 'Please enter the amount in USD',
                 hintStyle: TextStyle(color: Colors.grey),
                 prefixIcon: Icon(
                   Icons.monetization_on,
                   color: Colors.grey,
                 ),
-                filled: true,
-                fillColor: Colors.black),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2.0,
+                    color: Colors.grey,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
+                ),
+              ),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+            ),
           )
         ],
       ),
-    )));
+    ));
   }
 }
